715f0bca1c607733f6c52d96b19d43f82a7f6aa3
love-from-dingzhen <love-from-dingzhen@outlook.com>
2025-03-23 02:45:23 +0800

    Site updated: 2025-03-23 02:45:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1742669061225";const preCache = ["/images/taichi.png","/","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1742669122840";const preCache = ["/images/taichi.png","/","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
