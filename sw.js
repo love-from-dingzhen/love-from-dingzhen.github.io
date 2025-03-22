f34fe72afd4acfb52dfc4c5d4637959e922fe31e
love-from-dingzhen <love-from-dingzhen@outlook.com>
2025-03-22 20:54:48 +0800

    Site updated: 2025-03-22 20:54:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1742647474808";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1742648081079";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
